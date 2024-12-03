dc0cf06bc5e84f00ea41b6053bba9ff021e60603
Arbab-Mustafa <22-se-58@students.uettaxila.edu.pk>
2024-12-03 14:33:15 +0500

    fix bg

--- app/_component/Hero.jsx
+++ app/_component/Hero.jsx
@@ -4,11 +4,10 @@ import React, { useState, useEffect } from "react";
 const Hero = () => {
   // Array of image URLs
   const images = [
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbs2_0Z8xqnDN9TaF5N2pkO9yEKnsb-A9Z3SXxiZP6HeTbqW_ngQOjiNIjJc3QdArJF1s&usqp=CAU",
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDNWQ5rXzwxXYtO2eoa5W9vTaHSgBrYRVY1QP78RWE0nU3b1Tgm1SNQaqqmc9GcS4-9w&usqp=CAU",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9UM4bKMF1sT6AgedtbRFQBLRr1FUS6tMAqX9Rck6p8RLkmuAu9r94Dekg9EKLcJ0MEwo&usqp=CAU",
+
     "https://burgundydiamonds.com/wp-content/uploads/2023/08/Pink-yellow-and-white-diamonds.webp",
-    "https://via.placeholder.com/1920x1080?text=Image+4",
-    "https://via.placeholder.com/1920x1080?text=Image+5",
+    "https://png.pngtree.com/background/20231017/original/pngtree-vibrant-jewel-toned-diamonds-arranged-on-a-white-background-in-3d-picture-image_5585876.jpg",
   ];
 
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
@@ -19,7 +18,7 @@ const Hero = () => {
       setCurrentImageIndex((prevIndex) =>
         prevIndex === images.length - 1 ? 0 : prevIndex + 1
       );
-    }, 1000); // Change every second
+    }, 1500); // Change every second
 
     return () => clearInterval(interval); // Cleanup on unmount
   }, [images.length]);
@@ -27,24 +26,11 @@ const Hero = () => {
   return (
     <div>
       <section
-        className="relative bg-no-repeat bg-cover bg-center h-[80vh] transition-all duration-1000 ease-in-out"
+        className="relative bg-no-repeat bg-cover bg-center h-[70vh] transition-all duration-1000 delay-400 ease-in-out"
         style={{
           backgroundImage: `url(${images[currentImageIndex]})`,
         }}
-      >
-        <div className="absolute inset-0 bg-gray-900/50"></div>
-
-        <div className="relative mx-auto max-w-screen-xl px-4 py-32 sm:px-6 lg:flex lg:h-full lg:items-center lg:px-8">
-          <div className="max-w-xl text-center mx-auto">
-            <h1 className="text-white text-4xl font-bold">
-              Welcome to Our Dynamic Hero Section
-            </h1>
-            <p className="mt-4 text-gray-300">
-              Experience smooth transitions and vibrant backgrounds.
-            </p>
-          </div>
-        </div>
-      </section>
+      ></section>
     </div>
   );
 };
